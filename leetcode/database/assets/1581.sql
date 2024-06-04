9bfe6d0e81759d11e238e683b6bca86c4d394724
Klepov Yuri <onword333@gmail.com>
2024-06-04 16:38:43 +0300

    leetcode database 1581

---
+++ leetcode/database/assets/1581.sql
@@ -0,0 +1,15 @@
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT
+  customer_id,
+  COUNT(visit_id) AS count_no_trans
+FROM
+  Visits
+WHERE
+  visit_id NOT IN (
+    SELECT
+      visit_id
+    FROM
+      Transactions
+  )
+GROUP BY
+  customer_id
