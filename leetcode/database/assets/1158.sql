62a44976133a0783af738108c9ce4a87b0f1e7cc
Klepov Yuri <onword333@gmail.com>
2024-06-13 12:06:23 +0300

    leetcode database 1158

---
+++ leetcode/database/assets/1158.sql
@@ -0,0 +1,16 @@
+-- 1158. Market Analysis I
+SELECT
+  u.user_id AS buyer_id,
+  u.join_date,
+  COUNT(o.order_id) AS orders_in_2019
+FROM
+  Users u
+  LEFT JOIN Orders o ON u.user_id = o.buyer_id
+  AND EXTRACT(
+    year
+    from
+      o.order_date
+  ) = 2019
+GROUP BY
+  u.user_id,
+  u.join_date
