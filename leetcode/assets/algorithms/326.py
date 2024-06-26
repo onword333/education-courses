3e53901fc74581cf0f43fcddf97786235b4dbb60
Klepov Yuri <onword333@gmail.com>
2024-06-26 11:28:53 +0300

    leetcode algo 326

---
+++ leetcode/assets/algorithms/326.py
@@ -0,0 +1,18 @@
+"""
+326. Power of Three
+"""
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False        
+
+        while n % 3 == 0:
+            n //= 3
+        return n == 1
+
+
+n = 27
+obj = Solution()
+print(obj.isPowerOfThree(n))
